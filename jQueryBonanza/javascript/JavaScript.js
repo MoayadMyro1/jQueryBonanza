8b0f5d3d6c035c6b8a82dd75abe897d5372ec4d7
Moayad Myro <moayadmyro@gmail.com>
2022-04-18 06:36:47 -0700

    jQueryBonanza

---
+++ jQueryBonanza/javascript/JavaScript.js
@@ -0,0 +1,38 @@
+﻿jQuery(document).ready(function () {
+    $('#btnclicked').click(function () {
+        console.log("I click");
+    });
+    $('#btnfadeout').click(function () {
+        $('#txtfadeout').fadeOut(100);
+    });
+    $('#btnappend').click(function () {
+        $('#txtappend').append(" Test me");
+    });
+    $('#btnfadein').click(function () {
+        $('#txtfadein').hide().fadeIn();
+    });
+    $('#tbtnadd').click(function () {
+        $('#txtadd').addClass("rdtxt");
+    });
+    $('#btnhtml').click(function () {
+        console.log($('#txthtml').html());
+    });
+    $('#btntext').click(function () {
+        console.log($('#txttext').text());
+    });
+    $('#btnval').click(function () {
+        console.log($('#txtval').val());
+        console.log($('#btnval').val());                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+    });
+    $('#btnhide').click(function () {
+        $('#txthide').hide();
+    });
+    $('#btnshow').click(function () {
+        $('#txthide').show();
+    });
+    $('#btnslide').click(function () {
+        $('#txtslide').hide().slideDown("slow", function () {
+            // Animation complete.
+        });
+    });
+});
